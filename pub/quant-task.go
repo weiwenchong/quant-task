95e79b4505c880a3913334296c6b0e1c681df04a
weiwenchong14566 <weiwenchong14566@ipalfish.com>
2021-08-18 17:19:57 +0800

    init

---
+++ pub/quant-task.go
@@ -0,0 +1,26 @@
+package pub
+
+import (
+	"google.golang.org/grpc"
+	"log"
+)
+
+const PORT = ":10001"
+
+var Client TaskClient
+
+func init() {
+	conn, err := grpc.Dial(PORT, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Panicf("InitClient quant-order err:%v", err)
+	}
+	go func() {
+		defer func() {
+			log.Printf("conn close start")
+			conn.Close()
+			log.Printf("conn close")
+		}()
+		select{}
+	}()
+	Client = NewOrderClient(conn)
+}
